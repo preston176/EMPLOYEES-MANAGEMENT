30e9069d6e13f4c6d4a230816a0c25c7c052d46c
preston176 <prestonnyamweya@gmail.com>
2023-07-26 02:42:33 +0300

    states

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './style.css'
+
+const Login = () => {
+    const [values, setValues] =useState({
+        email: '',
+        password: ''
+    })
+
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+      <div className="bg-white p-3 rounded w-25 border">
+        <h2>Login Page</h2>
+        <form>
+            <div className="mb-3">
+                <label htmlFor="email">
+                    <strong>Email</strong>
+                </label>
+                <input onChange={e => {setValues({...values,email: e.target.value })}}  type="email" placeholder='Enter Email' name='email' className='form-control rounded-0' />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password">
+                    <strong>Password</strong>
+                </label>
+                <input onChange={e => {setValues({...values, password: e.target.value })}} type="password" placeholder='Enter Password' name='password' className='form-control rounded-0' />
+            </div>
+            <button type='submit' className='btn btn-success w-100 rounded-0'>
+                Log in 
+            </button>
+            <p>You agree to our T&C</p>
+            <button className='btn btn-default border w-100 bg-light rounded-0 text-decoration-underline'>
+                Create Account
+            </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
+  
