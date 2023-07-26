b5ccd618efe8aa1f67ea82d499216ab7e9bc19ab
preston176 <prestonnyamweya@gmail.com>
2023-07-26 15:31:24 +0300

    employee list

--- frontend/src/Employee.jsx
+++ frontend/src/Employee.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Employee = () => {
   return (
-    <div>
-      emp
+    <div className='px-5 py-3'>
+      <div className="d-flex justify-content-center">
+        <h3>Employee List</h3>
+      </div>
+      <button className='btn btn-success'>Add Employee</button>
     </div>
   )
 }
